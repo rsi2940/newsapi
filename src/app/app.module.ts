999b10208bef5fc759b18751aaaf41aa0db7de7f
rsi2940 <rishi2940@gmail.com>
2018-08-07 23:25:47 -0400

    created new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HeadingsComponent } from './headings/headings.component';
 import { FooterComponent } from './footer/footer.component';
+import { ScienceComponent } from './science/science.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { SportsComponent } from './sports/sports.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HeadingsComponent, FooterComponent],
+  declarations: [AppComponent, NavComponent, HeadingsComponent, FooterComponent, ScienceComponent, TechnologyComponent, SportsComponent],
   imports: [BrowserModule, HttpClientModule, MDBBootstrapModule.forRoot()],
   schemas: [NO_ERRORS_SCHEMA],
   providers: [],
