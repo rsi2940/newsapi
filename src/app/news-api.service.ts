49351a58f8f50cfa736bd6d1d2e2cd4058766124
rsi2940 <rishi2940@gmail.com>
2020-07-06 14:51:00 -0400

    blah blah

--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,15 +7,7 @@ export class NewsApiService {
   private apiUrl = 'https://api.nytimes.com/svc/topstories/v2/';
   private apiKey = '.json?api-key=YNGrW2g9HqBo1ilYzdbS52vUAWRAJtFj';
   private apiCategory = 'us';
-  constructor(private http: HttpClient) {}
-  // getNews(category = this.apiCategory) {
-  //   this.http
-  //     .get(`${this.apiUrl}${category}${this.apiKey}`)
-  //     .subscribe((data: any) => {
-  //       // console.log(data);
-  //       return data.results;
-  //     });
-  // }
+  constructor() {}
 
   async getNews(category = this.apiCategory) {
     const response = fetch(this.apiUrl + category + this.apiKey);
